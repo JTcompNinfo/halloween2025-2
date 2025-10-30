12c2b7f3c40dbcd093d06792fb387bc75cd44eb9
JTcompNinfo <cte-joel.dinovo@sllboces.org>
2025-10-30 12:54:39 +0000

    18:03

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+      navClose = document.getElementById('nav-close');
 
 /* Menu show */
 if(navToggle){
     navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
+        navMenu.classList.add('show-menu');
     })
 }
 
@@ -15,9 +15,17 @@ if(navToggle){
 /* Menu hidden */
 if(navClose){
     navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
+        navMenu.classList.remove('show-menu');
     })
 }
+
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
