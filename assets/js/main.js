94bc1d29b288c998ca9be2534d994b5c8fecb1a7
JTcompNinfo <cte-joel.dinovo@sllboces.org>
2025-10-31 14:23:28 +0000

    done ish enough

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,13 +27,58 @@ const linkAction = () => {
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/
 
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+                       : header.classList.remove('bg-header')
+}
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true // animations repeat
+})
+
+sr.reveal(`.home__data, .footer__content, .footer__logo, .footer__description`)
+sr.reveal(`.home__tree-1`, {origin: 'left', delay: 800})
+sr.reveal(`.home__tree-2`, {origin: 'right', delay: 800})
+sr.reveal(`.home__img`, {delay: 800})
+sr.reveal(`.category__card, .about__data`, {interval: 100})
+sr.reveal(`.about__img, .about__data .footer__tree-2`, {origin: 'left'})
+sr.reveal(`.party__images, .party__data .footer__tree-1`, {origin: 'right'})
